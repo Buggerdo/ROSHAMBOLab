7b4fd13c93a5af55a6a6d5e875f774c716df56cc
Troy <wilson.troy.anthony@gmail.com>
2022-08-06 12:58:49 -0400

    Add project files.

---
+++ ROSHAMBO/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ROSHAMBO
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Roshambo test = Roshambo.paper;
+            Console.WriteLine((int)test);
+            int testInt = 0;
+            Console.WriteLine((Roshambo)testInt);
+
+            test.ToString();
+            Console.WriteLine(test);
+
+            string test2 = "tea";
+            Roshambo getProduct;
+            bool checkParse = Enum.TryParse(test2, out getProduct);
+            Console.WriteLine(checkParse);
+            Console.WriteLine(getProduct);
+        }
+    }
+}
---
+++ ROSHAMBO/Roshambo.cs
@@ -0,0 +1,9 @@
+﻿namespace ROSHAMBO
+{
+    public enum Roshambo
+    {
+        rock = 0,
+        paper = 1,
+        scissors = 2
+    }
+}

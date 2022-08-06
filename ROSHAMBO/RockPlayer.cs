517f37e990ef862c0161431dedda4b82e60ee21c
Troy <wilson.troy.anthony@gmail.com>
2022-08-06 14:20:03 -0400

    Added RockPlayer

---
+++ ROSHAMBO/RockPlayer.cs
@@ -0,0 +1,15 @@
+﻿namespace ROSHAMBO
+{
+    public class RockPlayer : Player
+    {
+        public RockPlayer()
+        {
+            Name = "The Rock";
+        }
+
+        public override Roshambo GenerateRoshambo()
+        {
+            return Roshambo.rock;
+        }
+    }
+}

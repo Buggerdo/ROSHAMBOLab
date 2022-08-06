187166e6b7b2a3b22940dd0470b9cd0c5d7a9533
Troy <wilson.troy.anthony@gmail.com>
2022-08-06 14:36:18 -0400

    Added constructor

--- ROSHAMBO/HumanPlayer.cs
+++ ROSHAMBO/HumanPlayer.cs
@@ -2,5 +2,22 @@
 {
     internal class HumanPlayer : Player
     {
+        public HumanPlayer()
+        {
+            do
+            {
+                Console.Write("Please enter your name: ");
+                string input = Console.ReadLine();
+                if(!String.IsNullOrEmpty(input))
+                {
+                    Name = input;
+                    return;
+                } 
+            } while(true);
+        }
+        public override Roshambo GenerateRoshambo()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ROSHAMBO/Program.cs
+++ ROSHAMBO/Program.cs
@@ -4,19 +4,7 @@
     {
         static void Main()
         {
-            Roshambo test = Roshambo.paper;
-            Console.WriteLine((int)test);
-            int testInt = 0;
-            Console.WriteLine((Roshambo)testInt);
-
-            test.ToString();
-            Console.WriteLine(test);
-
-            string test2 = "tea";
-            Roshambo getProduct;
-            bool checkParse = Enum.TryParse(test2, out getProduct);
-            Console.WriteLine(checkParse);
-            Console.WriteLine(getProduct);
+            Startup _ = new();
         }
     }
 }

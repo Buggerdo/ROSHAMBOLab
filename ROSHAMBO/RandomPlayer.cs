758f5b68e8add48c72cb2f66284e261ccc19fe3a
Troy <wilson.troy.anthony@gmail.com>
2022-08-06 14:19:54 -0400

    Added RandomPlayer

---
+++ ROSHAMBO/RandomPlayer.cs
@@ -0,0 +1,17 @@
+﻿namespace ROSHAMBO
+{
+    internal class RandomPlayer : Player
+    {
+        public RandomPlayer()
+        {
+            Name = "Randy Random";
+        }
+
+        public override Roshambo GenerateRoshambo()
+        {
+            Random rand = new();
+            int randInt = rand.Next(3);
+            return (Roshambo)randInt;
+        }
+    }
+}
